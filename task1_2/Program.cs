d7998b6457f3a4339483c8560c748f4c15646504
Vladimir Greyner <greynervladimir2005@mail.ru>
2024-03-03 02:45:45 +0300

    Complete task1_2

--- task1_2/Program.cs
+++ task1_2/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 7 == 0 && a % 23 == 0) Console.WriteLine($"Число {a} кратно 7 и 23");
+else Console.WriteLine($"Число {a} не кратно 7 и 23 одновременно");
