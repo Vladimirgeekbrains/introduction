3895aecd6c8a9226bbe63a4c28ecf6f7b07187da
Vladimir Greyner <greynervladimir2005@mail.ru>
2024-03-03 02:18:58 +0300

    Done 1st task

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,18 @@
-﻿int a = 5;
-Console.WriteLine(a);
+﻿Console.WriteLine("Input first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+      
+Console.WriteLine("Input second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+     Console.WriteLine("First number is greater");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine("Second number is greater");
+}
+else
+{
+    Console.WriteLine("Numbers are equal");
+}
