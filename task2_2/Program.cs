6334377e86c9b07185f09585cdc03c9ce40d6c49
Vladimir Greyner <greynervladimir2005@mail.ru>
2024-03-03 03:06:02 +0300

    Completed task2_2

--- task2_2/Program.cs
+++ task2_2/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.WriteLine("Введите целое число - координату x:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое число - координату y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0) Console.WriteLine("Первая четверть");
+
+if (x > 0 && y < 0) Console.WriteLine("Четвёртая четверть");
+
+if (x < 0 && y > 0) Console.WriteLine("Вторая четверть");
+
+if (x < 0 && y < 0) Console.WriteLine("Третья четверть");
+
+if (x == 0 || y == 0) Console.WriteLine("Неккоректный ввод");
+
+
+
