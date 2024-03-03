9daa1b23f685ef5a5d5d70ae04eace82693b7734
Vladimir Greyner <greynervladimir2005@mail.ru>
2024-03-03 03:25:23 +0300

    Completed task2_3

--- task2_3/Program.cs
+++ task2_3/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine("Введите целое двузначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10 || num > 99) Console.WriteLine("Некорректный ввод!");
+else
+{
+    int FirstDigit = num / 10;
+    int LastDigit = num % 10;
+    //Было замечено, что первое число может быть нулём
+    if (FirstDigit > LastDigit) Console.WriteLine("Первая цифра больше");
+    else if (FirstDigit < LastDigit) Console.WriteLine("Последняя цифра больше");
+    else Console.WriteLine("Цифры одинаковы");
+}
