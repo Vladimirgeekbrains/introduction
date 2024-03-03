3f893fb5cc94206198c504bfa2dd73382734bb24
Vladimir Greyner <greynervladimir2005@mail.ru>
2024-03-03 03:35:09 +0300

    Добавил условия задач

--- task1_2/Program.cs
+++ task1_2/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите целое число:");
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите целое число:");
 int a = Convert.ToInt32(Console.ReadLine());
 if (a % 7 == 0 && a % 23 == 0) Console.WriteLine($"Число {a} кратно 7 и 23");
 else Console.WriteLine($"Число {a} не кратно 7 и 23 одновременно");
