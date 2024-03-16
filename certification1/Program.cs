6ea9872700b5ba78ac1c6716a910b2974fd2e17e
Vladimir Greyner <greynervladimir2005@mail.ru>
2024-03-16 05:30:09 +0300

    Done task

---
+++ certification1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+;
+
+void ShowNumberInterval(int m, int n)
+{
+    Console.Write($"{m} ");
+    if (m > n) m--;
+    else if (n > m) m++;
+    else return;
+    ShowNumberInterval(m, n);
+    return;
+}
+
+Console.WriteLine("Введите число M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+ShowNumberInterval(m, n);
