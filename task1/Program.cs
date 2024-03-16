79a3e487cd727996770f7d2ac3e4480c92ae5c7c
Vladimir Greyner <greynervladimir2005@mail.ru>
2024-03-16 04:48:29 +0300

    Try to do task 1_3

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,18 +1,24 @@
-﻿Console.WriteLine("Input first number");
-int num1 = Convert.ToInt32(Console.ReadLine());
+﻿// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
       
-Console.WriteLine("Input second number");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2)
-{
-     Console.WriteLine("First number is greater");
-}
-else if (num1 < num2)
-{
-    Console.WriteLine("Second number is greater");
-}
-else
-{
-    Console.WriteLine("Numbers are equal");
-}
+// if (num1 > num2)
+// {
+//      Console.WriteLine("First number is greater");
+// }
+// else if (num1 < num2)
+// {
+//     Console.WriteLine("Second number is greater");
+// }
+// else
+// {
+//     Console.WriteLine("Numbers are equal");
+// }
+
+int[,] arr = new int[2,3] {
+    {1, 2, 3},
+    {4, 5, 6}
+};
+Console.WriteLine(arr[0,0] + " " + arr[1, 1]);
