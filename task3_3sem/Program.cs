63e3c04018024038c6ec9041a9f0f33106298dd5
Vladimir Greyner <greynervladimir2005@mail.ru>
2024-03-17 03:44:42 +0300

    done task3_3

---
+++ task3_3sem/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+
+bool IsPalindrome(string str)
+{
+    bool flag = true;
+    for (int i = 0; i < str.Length/2; i++)
+    {
+        if (str[i] != str[str.Length - 1 - i]) flag = false;
+    }
+    return flag;
+}
+
+string notPalindrome = "adsdes,f";
+string palindrome = "Abc.,!,.cbA";
+
+
+if (IsPalindrome(notPalindrome)) Console.WriteLine("String is palindrome!");
+else Console.WriteLine("String is not palindrome!");
+
+
+if (IsPalindrome(palindrome)) Console.WriteLine("String is palindrome!");
+else Console.WriteLine("String is not palindrome!");
