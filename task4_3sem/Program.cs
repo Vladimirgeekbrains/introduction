f59d014e4dcc91d8a899ec3af5d07c24e8bf3569
Vladimir Greyner <greynervladimir2005@mail.ru>
2024-03-17 03:56:37 +0300

    done task 4_3

---
+++ task4_3sem/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. 
+// Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+
+string ReverseWords(string str)
+{
+    // string result = string.Empty;
+    string[] result = str.Split(' ');
+    string temp = "";
+    for (int i = 0; i < result.Length/2; i++)
+    {
+        temp = result[i];
+        result[i] = result[result.Length - 1 - i];
+        result[result.Length - 1 -i] = temp;
+    }
+    return string.Join(' ', result);
+}
+
+string str = "Hello world! How are you?";
+
+Console.WriteLine(ReverseWords(str));
