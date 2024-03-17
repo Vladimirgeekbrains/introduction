38672231357eab2234664846ef218e2f5a541619
Vladimir Greyner <greynervladimir2005@mail.ru>
2024-03-17 03:32:20 +0300

    done task

---
+++ task2_3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string start = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+string result = start.ToLower();
+Console.WriteLine(start);
+Console.WriteLine(result);
