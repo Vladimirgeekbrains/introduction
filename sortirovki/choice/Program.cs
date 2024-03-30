ee2b96cc1276fa772b6d4408c7e7b10a8fae6851
Vladimir Greyner <greynervladimir2005@mail.ru>
2024-03-30 15:12:59 +0300

    Added Sortirovki

---
+++ sortirovki/choice/Program.cs
@@ -0,0 +1,40 @@
+﻿//Сортировка выбором О(n^2)
+
+
+int[] SortVibor(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int indexMin = i;
+        for (int j = i; j < array.Length; j++)
+        {
+            if (array[j] < array[indexMin])
+                indexMin = j;
+        }
+        if (array[indexMin] == array[i])
+            continue;
+        int temp = array[i];
+        array[i] = array[indexMin];
+        array[indexMin] = temp;
+    }
+
+    return array;
+}
+
+
+void Input(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= new Random().Next(-30, 31); //[-30; 31)    [-30; 30]
+    }
+
+}
+
+Console.Write("Ввведите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] arr = new int[n];
+Input(arr);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", arr)}] ");
+Console.WriteLine($"Конечный массив: [{ string.Join(", ", SortVibor(arr)) }] ");
